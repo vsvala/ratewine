46565495f70855cc54e40f41f58131565c527335
Virva <virva.svala@gmail.com>
2018-09-15 00:44:12 +0300

    tehtävä 14

--- app/models/wineyard.rb
+++ app/models/wineyard.rb
@@ -1,5 +1,19 @@
 class Wineyard < ActiveRecord::Base
   has_many :wines, dependent: :destroy
+  has_many :ratings, through: :wines
+
+
+  def average_rating
+    "Has #{ratings.count} #{pluralize_help}, average #{ratings.average(:score)}" 
+  end
+
+  def pluralize_help
+    if ratings.count !=0
+      ratings.count == 1 ? "rating" : "rating".pluralize
+    end
+  end
+
+
 end
 
 
