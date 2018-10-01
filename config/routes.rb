4aabef33b25d7eb3cd1f488020ce6a29c8a68771
Virva <virva.svala@gmail.com>
2018-10-01 22:55:07 +0300

    VKO5 tehtava 1 beer api

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
+  get 'places', to: 'places#index'
+  post 'places', to:'places#search'
   # get 'ratings', to: 'ratings#index'
   # get 'ratings/new', to:'ratings#new'
   # post 'ratings', to: 'ratings#create'
