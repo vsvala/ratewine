9148ca8f9a622bd6843428217ad731e218dea127
Virva <virva.svala@gmail.com>
2018-10-07 21:28:17 +0300

    tehtävä 5 ulkoasun säätöä

--- lib/weathermapping_api.rb
+++ lib/weathermapping_api.rb
@@ -1,10 +1,10 @@
 class WeathermappingApi
   def self.weather_in(city)
     url = "http://api.apixu.com/v1/current.xml?key=#{key}&q=#{city}"
-    #url = "http://api.apixu.com/v1/current.json?key=#{ENV['APIXU_APIKEY']}&q=#{city}"
+    # url = "http://api.apixu.com/v1/current.json?key=#{ENV['APIXU_APIKEY']}&q=#{city}"
 
     response = HTTParty.get(url)
-    #response.parsed_response['current']
+    # response.parsed_response['current']
     weathers = response.parsed_response["root"]["current"]
 
     return [] if weathers.is_a?(Hash) && weathers['last_updated'].nil?
