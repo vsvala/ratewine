059b2985d67f94103e98532eeb5c76bf7ae8c4d0
Virva <virva.svala@gmail.com>
2018-10-18 19:04:12 +0300

    extend top rb poisto

--- app/models/concerns/top.rb
+++
@@ -1,9 +0,0 @@
-# module Top
-#   extend ActiveSupport::Concern
-#   include RatingAverage
-
-#   def self.top(how_many)
-#     sorted_by_rating_in_desc_order = all.sort_by{ |b| -(b.average_rating || 0) }
-#     sorted_by_rating_in_desc_order[0, how_many]
-#   end
-# end
